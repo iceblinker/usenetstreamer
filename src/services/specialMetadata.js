5f7baf81ee2ad8c1b8f23af889f53b1430ca216e
iceblinker <iceblinkluck@gmail.com>
2026-01-18 11:30:22 -0800

    Add specialMetadata stub (telemetry removed)

---
+++ src/services/specialMetadata.js
@@ -0,0 +1,9 @@
+// Stub for specialMetadata - telemetry removed for privacy
+// This provides minimal exports to satisfy server.js imports
+
+module.exports = {
+    specialCatalogPrefixes: [],
+    getSpecialMetadata: () => null,
+    isSpecialId: () => false,
+    resolveSpecialId: async () => null,
+};
